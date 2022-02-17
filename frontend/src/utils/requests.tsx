fda90fa2a924681d228031e5d1f408afef5e63c7
leobiriba <leonardoxcontato@gmail.com>
2022-02-17 19:09:22 -0300

    Configurando backend

--- frontend/src/utils/requests.tsx
+++ frontend/src/utils/requests.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost/bitstore_desafio/bitstore/backend/src/main/com.bitstore.db/";
+export const BASE_URL = "http://localhost/backend/";
 
-export const BASE_URL_RELATORIO = "http://localhost/bitstore_desafio/bitstore/backend/src/main/com.bitstore.relatorio"
+export const BASE_URL_RELATORIO = "http://localhost/backend/relatorio.php"
